4e049e64f17b3a649073a02a6e4ecf7e8b79b1ae
jailson <jailsonrlima@hotmail.com>
2023-02-19 15:43:12 -0300

    Criado pacote biblioteca

---
+++ src/Fundamentos/biblioteca/livro.java
@@ -0,0 +1,27 @@
+package Fundamentos.biblioteca;
+
+public class livro {
+private String titulo;
+private String autor;
+private int pagina;
+public String getTitulo() {
+	return titulo;
+}
+public void setTitulo(String titulo) {
+	this.titulo = titulo;
+}
+public String getAutor() {
+	return autor;
+}
+public void setAutor(String autor) {
+	this.autor = autor;
+}
+public int getPagina() {
+	return pagina;
+}
+public void setPagina(int pagina) {
+	this.pagina = pagina;
+}
+
+
+}
